55ded911a3add49d0d54d5f8bb3bf24bfc61c0b3
vitormtz <vitor.brak@hotmail.com>
2025-12-05 20:27:05 -0300

    refactor: separate password fields from profile update DTO

--- web-app/src/main/java/com/example/petsafeweb/dto/UpdateProfileRequest.java
+++ web-app/src/main/java/com/example/petsafeweb/dto/UpdateProfileRequest.java
@@ -1,13 +1,13 @@
 package com.example.petsafeweb.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * DTO para requisição de atualização de perfil
+ * DTO para requisição de atualização de perfil (nome e telefone)
+ * O mapeamento snake_case é feito automaticamente pelo ObjectMapper
  */
 @Data
 @Builder
@@ -15,15 +15,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UpdateProfileRequest {
 
-    @JsonProperty("full_name")
     private String fullName;
-
-    @JsonProperty("phone")
     private String phone;
-
-    @JsonProperty("current_password")
-    private String currentPassword;
-
-    @JsonProperty("new_password")
-    private String newPassword;
 }
